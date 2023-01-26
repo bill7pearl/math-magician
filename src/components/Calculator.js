464740899a693ba9b77871bcb951620bdabe41b0
Billal <billal7chami@gmail.com>
2023-01-26 19:33:38 +0100

    Fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,25 +20,25 @@ const Calculator = () => {
     <main className="wrapper">
       <div className="screen">{getResult()}</div>
       <div className="buttonBox">
-        <button className="button" onClick={onClickBtn}>AC</button>
-        <button className="button" onClick={onClickBtn}>+/-</button>
-        <button className="button" onClick={onClickBtn}>%</button>
-        <button className="button opt" onClick={onClickBtn}>&divide;</button>
-        <button className="button" onClick={onClickBtn}>7</button>
-        <button className="button" onClick={onClickBtn}>8</button>
-        <button className="button" onClick={onClickBtn}>9</button>
-        <button className="button opt" onClick={onClickBtn}>x</button>
-        <button className="button" onClick={onClickBtn}>4</button>
-        <button className="button" onClick={onClickBtn}>5</button>
-        <button className="button" onClick={onClickBtn}>6</button>
-        <button className="button opt" onClick={onClickBtn}>-</button>
-        <button className="button" onClick={onClickBtn}>1</button>
-        <button className="button" onClick={onClickBtn}>2</button>
-        <button className="button" onClick={onClickBtn}>3</button>
-        <button className="button opt" onClick={onClickBtn}>+</button>
-        <button className="button zero" onClick={onClickBtn}>0</button>
-        <button className="button" onClick={onClickBtn}>.</button>
-        <button className="button equals" onClick={onClickBtn}>=</button>
+        <button type="button" className="button" onClick={onClickBtn}>AC</button>
+        <button type="button" className="button" onClick={onClickBtn}>+/-</button>
+        <button type="button" className="button" onClick={onClickBtn}>%</button>
+        <button type="button" className="button opt" onClick={onClickBtn}>&divide;</button>
+        <button type="button" className="button" onClick={onClickBtn}>7</button>
+        <button type="button" className="button" onClick={onClickBtn}>8</button>
+        <button type="button" className="button" onClick={onClickBtn}>9</button>
+        <button type="button" className="button opt" onClick={onClickBtn}>x</button>
+        <button type="button" className="button" onClick={onClickBtn}>4</button>
+        <button type="button" className="button" onClick={onClickBtn}>5</button>
+        <button type="button" className="button" onClick={onClickBtn}>6</button>
+        <button type="button" className="button opt" onClick={onClickBtn}>-</button>
+        <button type="button" className="button" onClick={onClickBtn}>1</button>
+        <button type="button" className="button" onClick={onClickBtn}>2</button>
+        <button type="button" className="button" onClick={onClickBtn}>3</button>
+        <button type="button" className="button opt" onClick={onClickBtn}>+</button>
+        <button type="button" className="button zero" onClick={onClickBtn}>0</button>
+        <button type="button" className="button" onClick={onClickBtn}>.</button>
+        <button type="button" className="button equals" onClick={onClickBtn}>=</button>
       </div>
     </main>
   );
