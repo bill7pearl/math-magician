f3df389bcc19f330847732726d1668bf4ebc39f3
Billal <billal7chami@gmail.com>
2023-02-02 18:21:25 +0100

    Added Home & Quote pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import Calculator from './components/Calculator';
+import Home from './components/Home';
+import Quote from './components/Quote';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <nav>
+        <div className="header">
+          <h1 className="title">Math Magicians</h1>
+
+          <ul>
+            <li><Link to="/" className="no-border">Home</Link></li>
+            <li><Link to="/Calculator">Calculator</Link></li>
+            <li><Link to="/Quote">Quote</Link></li>
+          </ul>
+        </div>
+      </nav>
+      <div className="footer"><p>Math Magicians © 2023</p></div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Calculator" element={<Calculator />} />
+        <Route path="/Quote" element={<Quote />} />
+      </Routes>
+    </>
   );
 }
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Home = () => (
+  <main>
+    <div />
+    <div className="home_page">
+      <h2>Welcome to our page!</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Saepe deserunt blanditiis voluptates aliquam maiores consequatur facere nam magnam.
+        Numquam voluptate possimus distinctio! Sunt dicta in culpa illo accusamus,
+        consectetur voluptates laudantium. In harum quisquam amet ut quis sed repella
+        t quia maiores? Dolor placeat, odio possimus velit corrupti voluptas natus tempora
+        eligendi aspernatur minima? Laudantium officiis sit similique necessitatibus nisi
+        laboriosam sapiente rerum dicta, mollitia, dolore, molestias labore culpa sequi mod
+        i ullam in? Natus, obcaecati. Qui sit unde, doloremque quibusdam possimus vitae aper
+        iam, voluptatibus voluptatem eligendi dolor amet velit. Pariatur ipsam consequuntur
+        nesciunt eum voluptatum. Autem sit in rem temporibus quisquam?
+      </p>
+    </div>
+  </main>
+);
+export default Home;
