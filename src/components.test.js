ae452480f1235f8b290cf1e2cd61b7c01d5eef5b
Billal <billal7chami@gmail.com>
2023-02-02 15:12:36 +0100

    Fix linters

--- src/components.test.js
+++ src/components.test.js
@@ -1,44 +1,44 @@
-import renderer from "react-test-renderer";
-import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
+import renderer from 'react-test-renderer';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 
-import App from "./App";
+import App from './App';
 
-describe("test rendering", () => {
-  test("test if App components render correctly", () => {
+describe('test rendering', () => {
+  test('test if App components render correctly', () => {
     const tree = renderer.create(<App />).toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
 
-describe("Test Calculator interaction", () => {
-  test("test buttons interactions with screen", () => {
+describe('Test Calculator interaction', () => {
+  test('test buttons interactions with screen', () => {
     render(<App />);
-    const btnCalculator = document.querySelector(".button");
+    const btnCalculator = document.querySelector('.button');
     fireEvent.click(btnCalculator);
-    const result = screen.getAllByText("0")[0];
-    const btnZero = screen.getAllByText("0")[1];
+    const result = screen.getAllByText('0')[0];
+    const btnZero = screen.getAllByText('0')[1];
 
-    fireEvent.click(screen.getByText("2"));
+    fireEvent.click(screen.getByText('2'));
     fireEvent.click(btnZero);
-    fireEvent.click(screen.getByText("2"));
-    fireEvent.click(screen.getByText("3"));
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('3'));
 
-    expect(result).toHaveTextContent("2023");
+    expect(result).toHaveTextContent('2023');
   });
 
-  test("test addition with calculations", () => {
+  test('test addition with calculations', () => {
     render(<App />);
-    const btnCalculator = document.querySelector(".button");
+    const btnCalculator = document.querySelector('.button');
     fireEvent.click(btnCalculator);
-    const result = screen.getAllByText("0")[0];
-    fireEvent.click(screen.getByText("AC"));
-    fireEvent.click(screen.getByText("5"));
-    fireEvent.click(screen.getByText("+"));
-    fireEvent.click(screen.getByText("1"));
-    fireEvent.click(screen.getByText("2"));
-    fireEvent.click(screen.getByText("="));
+    const result = screen.getAllByText('0')[0];
+    fireEvent.click(screen.getByText('AC'));
+    fireEvent.click(screen.getByText('5'));
+    fireEvent.click(screen.getByText('+'));
+    fireEvent.click(screen.getByText('1'));
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('='));
     expect(result).toHaveTextContent((5 + 12).toString());
   });
 });
