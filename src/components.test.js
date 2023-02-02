4e54282d922664a5185d16e7ee6244143ebb1655
Billal <billal7chami@gmail.com>
2023-02-02 12:33:17 +0100

    Added calculator test cases

---
+++ src/components.test.js
@@ -0,0 +1,30 @@
+import renderer from 'react-test-renderer';
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import App from './App';
+
+describe('test rendering', () => {
+  test('test if App components render correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Test Calculator interaction', () => {
+  test('test buttons interactions with screen', () => {
+    render(<App />);
+    const btnCalculator = document.querySelector('.button');
+    fireEvent.click(btnCalculator);
+    const result = screen.getAllByText('0')[0];
+    const btnZero = screen.getAllByText('0')[1];
+
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(btnZero);
+    fireEvent.click(screen.getByText('2'));
+    fireEvent.click(screen.getByText('3'));
+
+    expect(result).toHaveTextContent('2023');
+  });
+});
